2483506929efd36c14a1d7b4278e55bcb6166383
Shishqa <shishatskii.mn@phystech.edu>
2020-08-04 01:51:59 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from modules.loader.loader import load_data
+from modules.preprocessor.preprocessor import preprocess_data
+from modules.fitter.fitter import fit_model
+from modules.evaluator.evaluator import evaluate_model
+
+if __name__ == "__main__":
+    print("$ loading data...")
+    load_data()
+
+    print("$ preprocessing data...")
+    preprocess_data()
+
+    print("$ fitting model...")
+    fit_model()
+
+    print("$ evaluating model...")
+    evaluate_model()
