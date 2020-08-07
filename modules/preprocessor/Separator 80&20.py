87b34977e9568d2399ca60e49187d9e0e37eb6e3
Nikfilk2030 <Nikfilk2030@gmail.com>
2020-08-07 12:16:22 +0700

    added data shuffler, sepatation procentage and файл нейм при вызове функции

--- modules/preprocessor/Separator 80&20.py
+++ modules/preprocessor/Separator 80&20.py
@@ -2,13 +2,14 @@ import numpy as np
 import pandas as pd
 
 
-def data_separator():
-    file_name = 'corpus_sent.csv'
+def data_separator(file_name, separation_percentage):
+    separation_percentage /= 100
+    # file_name = "../../data/corpus_sent.csv" для тестов, надо стереть file_name из начала функции чтобы тестить
     df = pd.read_csv(file_name, delimiter=',')  # читаю файл с примерами
     df = df.drop(['Unnamed: 0'], axis=1)  # убираю столбец с нумирацией
-    train = df.iloc[:25816]
-    test = df.iloc[25816:32272]
+    df = df.sample(frac=1).reset_index(drop=True)  # перемешиваю датафрейм
+    len_train = round(len(df) * separation_percentage)  # Длина тренировочного списка
+    len_test = len(df) - len_train  # Длина тестировочного списка
+    train = df.iloc[:len_train]
+    test = df.iloc[len_train:len_test]
     return train, test
-
-
-data_separator()
