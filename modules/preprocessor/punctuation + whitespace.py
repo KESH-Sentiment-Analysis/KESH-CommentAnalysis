e4c9219253b10e5c017d603a99b949b5fe62c49c
MAC <mac@MACs-MacBook-Air.local>
2020-08-07 11:42:05 +0700

    функции удаления пунктуации и пробелов

--- modules/preprocessor/punctuation + whitespace.py
+++ modules/preprocessor/punctuation + whitespace.py
@@ -1,18 +1,24 @@
 import string
 
 
-def remove_punctuation(listofcomments):
-    listofclearcomments = []
-    for comment in listofcomments:
+def remove_punctuation(list_of_comments):
+    # создаем список, где будут лежать обработанные комментарии
+    list_of_clear_comments = []
+    # пробегаемся по всем комментариям в сыром списке
+    for comment in list_of_comments:
         translator = str.maketrans('', '', string.punctuation)
-        # удаляем знаки препинания
+        # удаляем знаки препинания с помощью созданной функции трансляторя
         comment2 = comment.translate(translator)
-        listofclearcomments.append(comment2)
-    return listofclearcomments
+        # добавляем обработанный комментарий в список
+        list_of_clear_comments.append(comment2)
+    return list_of_clear_comments
 
 
-def remove_whitespace(listofcomments):
-    listofclearcomments = []
-    for comment in listofcomments:
-        listofclearcomments.append(" ".join(comment.split()))
-    return listofclearcomments
+def remove_whitespace(list_of_comments):
+    # создаем список, где будут лежать обработанные комментарии
+    list_of_clear_comments = []
+    # пробегаемся по всем комментариям в сыром списке
+    for comment in list_of_comments:
+        # обрабатываем и добавляем обработанный комментарий в список
+        list_of_clear_comments.append(" ".join(comment.split()))
+    return list_of_clear_comments
