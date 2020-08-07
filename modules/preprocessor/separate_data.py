949cfe187ed36052af1edd54be6391f07b81ece4
Shishqa <shishatskii.mn@phystech.edu>
2020-08-07 12:31:49 +0700

    refactored some code

--- modules/preprocessor/separate_data.py
+++ modules/preprocessor/separate_data.py
@@ -1,15 +1,21 @@
-import numpy as np
 import pandas as pd
 
 
 def data_separator(file_name, separation_percentage):
-    separation_percentage /= 100
     # file_name = "../../data/corpus_sent.csv" для тестов, надо стереть file_name из начала функции чтобы тестить
+
+    separation_percentage /= 100
+
     df = pd.read_csv(file_name, delimiter=',')  # читаю файл с примерами
-    df = df.drop(['Unnamed: 0'], axis=1)  # убираю столбец с нумирацией
+    df = df.drop(['Unnamed: 0'], axis=1)  # убираю столбец с нумeрацией
+
     df = df.sample(frac=1).reset_index(drop=True)  # перемешиваю датафрейм
-    len_train = round(len(df) * separation_percentage)  # Длина тренировочного списка
-    len_test = len(df) - len_train  # Длина тестировочного списка
+
+    # длины списков для тренировки и теста
+    len_train = round(len(df) * separation_percentage)
+    len_test = len(df) - len_train
+
     train = df.iloc[:len_train]
     test = df.iloc[len_train:len_test]
+
     return train, test
