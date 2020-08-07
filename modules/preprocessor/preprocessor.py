617c28a2d340c66b368d637e15d284ef33672594
Artorianus <karden1010@gmail.com>
2020-08-07 12:42:15 +0700

    Text processing

--- modules/preprocessor/preprocessor.py
+++ modules/preprocessor/preprocessor.py
@@ -4,7 +4,40 @@
 
 # импортируй здесь нужные тебе библиотеки
 # from ... import ...
+import re
+import nltk
+import string
+from nltk.corpus import stopwords
+from pymystem3 import Mystem
+from string import punctuation
 
-
-def preprocess_data():
-    """Ваш код тут"""
+def remove_numbers(text):
+    result = re.sub(r'\d+', '', text)
+    return result
+def remove_english(text):
+    results = list(text)
+    copyres = results.copy()
+    for result in results:
+        if result >= 'a' and result <= 'z':
+            copyres.remove(result)
+    return copyres
+def preprocess_data(text, lemmatiz_isTrue = True, tokenization_isTrue = True):
+    nltk.download("stopwords")
+    # Create lemmatizer and stopwords list
+    mystem = Mystem()
+    russian_stopwords = stopwords.words("russian")
+    text = text.lower()
+    text = remove_numbers(text)
+    text = remove_english(text)
+    strtext = ''.join([str(elem) for elem in text])
+    if lemmatiz_isTrue:
+        tokens = mystem.lemmatize(strtext)
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    tokens = [token for token in stripped if token not in russian_stopwords and token != " " and len(token.strip())]
+    if tokenization_isTrue:
+        return tokens
+    else:
+        strtext = " ".join(tokens)
+        return strtext
+print(preprocess_data('мне    понравилась та французская булочка с чаем fjifs2223?!'))
