9f164b40d08a9348ef4b0ff805db365cabc533e7
Artorianus <karden1010@gmail.com>
2020-08-08 12:42:00 +0700

    Connected Preprocessor

--- modules/preprocessor/preprocessor.py
+++ modules/preprocessor/preprocessor.py
@@ -5,7 +5,9 @@ import nltk
 import string
 from nltk.corpus import stopwords
 from pymystem3 import Mystem
-
+from modules.preprocessor.punctuation_and_whitespace import remove_punctuation
+from modules.preprocessor.punctuation_and_whitespace import remove_whitespace
+import pandas as pd
 
 def remove_garbage(text):
 
@@ -14,37 +16,54 @@ def remove_garbage(text):
     return result
 
 
-def preprocess_data(text, lemmatiz_isTrue = True, tokenization_isTrue = True):
-    nltk.download("stopwords")
+# removing rows with invalid labels
+def remove_bad_labels(data):
+    return data[data['labels'].isin(['negative', 'positive', 'neutral'])]
+
+
+def preprocess_data(data, lemmatiz_isTrue = True):
+
+    # removing invalid rows
+    data = remove_bad_labels(data)
+
+    # converting dataframe to list of strs
+    text_list = list(data['Text'])
+
     # Create lemmatizer and stopwords list
+    nltk.download("stopwords")
     mystem = Mystem()
     russian_stopwords = stopwords.words("russian")
-    
-    # lower case
-    text = text.lower()
-    
-    # removing numbers and english
-    text = remove_garbage(text)
-    
-    # lemmatization
-    if lemmatiz_isTrue:
-        tokens = mystem.lemmatize(text)
-    else:
-        tokens = text.split()
 
-    # removing punctuation and empty words   
-    table = str.maketrans('', '', string.punctuation)
-    stripped = [w.translate(table) for w in tokens]
-    tokens = [token for token in stripped if token not in russian_stopwords and token.strip()]
-    
-    # tokeniztion
-    if tokenization_isTrue:
-        return tokens
-    else:
-        text = " ".join(tokens)
-        return text
+    # Processing list of str
+    for i, v in enumerate(text_list):
+        # lower case
+        text_list[i] = text_list[i].lower()
+
+        # removing numbers and english
+        text_list[i] = remove_garbage(text_list[i])
+
+        # lemmatization
+        if lemmatiz_isTrue:
+            tokens = mystem.lemmatize(text_list[i])
+        else:
+            tokens = text_list[i].split()
+
+        # removing stopwords and empty words
+        tokens = [token for token in tokens if token not in russian_stopwords and token.strip()]
+
+        # converting list to str
+        text_list[i] = " ".join(tokens)
+
+    # removing punctuation and empty spaces
+    text_list = remove_punctuation(text_list)
+    text_list = remove_whitespace(text_list)
+
+    return text_list
 
 
 def Test():
-    print(preprocess_data("мне понравилась английская булочка с чаем!!??,,dffisj"))
+    label = ['positive', 'bebe']
+    texts =  ['банков 122 !!ds', 'dsek']
+    dataf = pd.DataFrame({'Text':texts, 'labels':label})
+    print(preprocess_data(dataf))
 #Test()
