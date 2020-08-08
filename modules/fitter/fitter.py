f7723f8b36f47d8b7ba7ac3731464f45c2d1ae72
Андрей Мордосевич <mordosevich1@gmail.com>
2020-08-08 13:32:11 +0700

    final_fit

--- modules/fitter/fitter.py
+++ modules/fitter/fitter.py
@@ -7,8 +7,9 @@ from sklearn import svm
 #from sklearn.metrics import recall_score
 
 def fit_model(X_train, Y_train, X_test, Y_test):
-    C, gamma, kernel = validator(examp.take(np.random.permutation(len(data_train))[:min(len(data_train),1000)-1]) #получение необходимых параметров
+    C, gamma, kernel = validator(X_train, Y_train) #получение необходимых параметров
     model = svm.SVC(kernel=kernel, gamma=gamma, C=C) #модель принимает необходимые параметры
     model.fit(X_train, Y_train) #модель обучается на тренировочных данных
-    evaluate_model(X_test, Y_test)
+    y_predict = model.predict(X_test) #определение принадлжености к группам тестовых данных относительно введённой нами модели
+    evaluate_model(y_predict, Y_test) #передача данных для сравнения в следующую функцию
 
