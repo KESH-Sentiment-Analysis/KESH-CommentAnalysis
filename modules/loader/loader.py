9f2855461e41bcc1759ec1277055337fe77468bb
Nikfilk2030 <Nikfilk2030@gmail.com>
2020-08-08 12:33:43 +0700

    upgraded loader and separate_data to final versions

--- modules/loader/loader.py
+++ modules/loader/loader.py
@@ -4,6 +4,16 @@
 
 # импортируй здесь нужные тебе библиотеки
 # from ... import ...
+import pandas as pd
+
 
 def load_data():
     """Ваш код тут"""
+
+
+def give_data(file_name):
+    # file_name = "../../data/corpus_sent.csv" #для тестов, надо стереть file_name из начала функции чтобы тестить
+    df = pd.read_csv(file_name, delimiter=',')  # читаю файл с примерами
+    df = df.drop(['Unnamed: 0'], axis=1)  # убираю столбец с нумирацией
+    data_frame = df.sample(frac=1).reset_index(drop=True)  # перемешиваю датафрейм
+    return data_frame
--- modules/preprocessor/separate_data.py
+++ modules/preprocessor/separate_data.py
@@ -1,21 +1,14 @@
 import pandas as pd
 
 
-def data_separator(file_name, separation_percentage):
-    # file_name = "../../data/corpus_sent.csv" для тестов, надо стереть file_name из начала функции чтобы тестить
-
+def data_separator(data_frame, separation_percentage):
     separation_percentage /= 100
 
-    df = pd.read_csv(file_name, delimiter=',')  # читаю файл с примерами
-    df = df.drop(['Unnamed: 0'], axis=1)  # убираю столбец с нумeрацией
-
-    df = df.sample(frac=1).reset_index(drop=True)  # перемешиваю датафрейм
-
     # длины списков для тренировки и теста
-    len_train = round(len(df) * separation_percentage)
-    len_test = len(df) - len_train
+    len_train = round(len(data_frame) * separation_percentage)
+    len_test = len(data_frame) - len_train
 
-    train = df.iloc[:len_train]
-    test = df.iloc[len_train:len_test]
+    train = data_frame.iloc[:len_train]
+    test = data_frame.iloc[len_train:len_test]
 
     return train, test
