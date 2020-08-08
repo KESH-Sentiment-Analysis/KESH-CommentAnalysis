c2e0cc339604d99dc5e2c71ff5a5dcaf466a43d2
MAC <mac@MACs-MacBook-Air.local>
2020-08-08 13:48:38 +0700

    connect

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from modules.loader.loader import load_data
+from modules.loader.loader import give_data
 from modules.preprocessor.preprocessor import preprocess_data
-from modules.fitter.fitter import fit_model
-from modules.evaluator.evaluator import evaluate_model
-import numpy as np
+from modules.preprocessor.vectorization import vectorizing
+from modules.preprocessor.separate_data import data_separator
+
 if __name__ == "__main__":
     print("$ loading data...")
-    load_data()
+    data_frame = give_data('corpus_sent.csv')
 
     print("$ preprocessing data...")
-    preprocess_data()
+    str_list = preprocess_data(data_frame)
+    matrix = vectorizing(str_list)
+    sep_vectors = data_separator(matrix)
+
 
     print("$ fitting model...")
     fit_model()
