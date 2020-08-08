fcfb0d3ee62a15f83945edb708e6ee668e9fba36
drovnikov <asdasd.ivanov2017@yandex.ru>
2020-08-08 11:22:22 +0700

    evaluation

--- modules/fitter/evaluation.py
+++ modules/fitter/evaluation.py
@@ -2,13 +2,16 @@
     Задача этого модуля -- обучение модели
 """
 from sklearn.metrics import classification_report
+import pandas as pd
 
 
-def evaluation_model(mas, y_pred):
+def evaluation_model(model, test):
     """Здесь будет ваша модель"""
+    for i in test:
+        y_pred.append(fit_model(i))
     y_true = []
-    for i in mas:
-        y_true.append(i[1])
+    model.drop(columns=0, inplace=True)
+    y_pred = model.array
     target_names = ['class 0 (positive)', 'class 1 (negative)', 'class 2 (neutral)']
     print(classification_report(y_true, y_pred, target_names=target_names))
 
