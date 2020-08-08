b9a8d4fb17ea90f359836ea234ea1478557e5607
Kilagen <kil4gen@gmail.com>
2020-08-08 13:10:50 +0700

    takes separated data

--- modules/fitter/validator.py
+++ modules/fitter/validator.py
@@ -5,17 +5,14 @@ from sklearn.model_selection import GridSearchCV
 
 
 
-def validator(data_train, param_grid = {'C': [0.1, 1, 5, 10], 'gamma': [0.5, 5, 10], 'kernel': ['rbf', 'linear']}):
-    """пример вызова функции validator()
-    C, gamma, kernel = validator(example, your_param_grid)"""
+def validator(x_train, y_train, param_grid = {'C': [0.1, 1, 5, 10], 'gamma': [0.5, 5, 10], 'kernel': ['rbf', 'linear']}):
+    """пример вызова функции validator():
+    C, gamma, kernel = validator(x_train, y_train, your_param_grid)"""
 
-    data_train = data_train[:min(len(data_train),1000) -1]
+    X_train = x_train[:min(len(x_train),1000) -1]
+    Y_train = y_train[:min(len(y_train),1000) -1]
 
     model = GridSearchCV(SVC(), param_grid)
-
-    # Разделение данных на фичи и цели
-    Y_train = data_train['Labels']
-    X_train = data_train.drop('Labels', axis=1)
 
     # Выбор лучших параметров для data_train
     model.fit(X_train, Y_train)
