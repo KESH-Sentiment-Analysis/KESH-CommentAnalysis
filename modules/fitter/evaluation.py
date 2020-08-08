c3a03f07a7b58b7389383dade023dab20fc61d8e
drovnikov <asdasd.ivanov2017@yandex.ru>
2020-08-08 13:08:54 +0700

    evaluate model

--- modules/fitter/evaluation.py
+++ modules/fitter/evaluation.py
@@ -4,15 +4,9 @@
 from sklearn.metrics import classification_report
 
 
-def evaluate_model(model, test_data):
-    """Здесь будет ваша модель"""
+def evaluate_model(model, test_x, test_y):
     target_names = ['class 0 (positive)', 'class 1 (negative)', 'class 2 (neutral)']
-    y_pred = model.predict(test_data['Text'].toarray)
-    print(classification_report(test_data['Labels'], y_pred, target_names=target_names))
+    y_pred = model.predict(test_x)
+    print(classification_report(test_y, y_pred, target_names=target_names))
 
 
-# 0 - positive
-# 1 - negative
-# 2 - neutral
-#mas = [('Ужасный товар!', 1), ('Рекомендую. Все супер!', 0), ('Здесь продают столы', 2)]
-#evaluation_model(mas, y_pred)
